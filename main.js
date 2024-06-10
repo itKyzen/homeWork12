1f6181a3084805ad560ac6f968cead4ab1cbc944
Dima Kuz <d.kuz.it@gmail.com>
2024-06-10 03:37:32 +0200

    create start app

---
+++ main.js
@@ -0,0 +1,4 @@
+let userName = prompt("Як твое им'я", 'Оксимирон');
+let userSurname = prompt("Як твое призвище", 'Оксимирон');
+let userAge = +prompt("Скока рокив", '35');
+document.write(`Я ${userName} ${userSurname} i менi ${userAge} рокiв. <br />`);
