ceba51aaf31a67a7288cfabf0acf918dd5d53af4
Dima Kuz <d.kuz.it@gmail.com>
2024-06-10 03:39:45 +0200

    finish  app

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 let userName = prompt("Як твое им'я", 'Оксимирон');
-let userSurname = prompt("Як твое призвище", 'Оксимирон');
+let userSurname = prompt("Як твое призвище", 'Янович');
 let userAge = +prompt("Скока рокив", '35');
 document.write(`Я ${userName} ${userSurname} i менi ${userAge} рокiв. <br />`);
+
+
+let number = +prompt("Введіть  число");
+let x = number.toString().split('');
+
+if (x.length === 5) {
+    for (let i = 0; i < x.length; i++) {
+        document.write(x[i] + "&nbsp;");
+    }
+} else {
+    document.write("Москаль!!!");
+}
